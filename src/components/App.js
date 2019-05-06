2c1f9f11c941c830a48957f5fabb4c335e3135cb
Kainan Ramos <Kainan.Ramos@vistajet.com>
2019-05-06 16:51:43 -0300

    [new feature/test] - adds a new gift to state when clicking the dd gift button

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
   state = {
     gifts: [],
   }
 
+  addGift = () => {
+    const { gifts } = this.state;
+    const newGifts = [...gifts];
+
+    const nextId = gifts.length > 0 ? gifts.length : 0;
+
+    newGifts.push({ id: nextId + 1 });
+
+    this.setState({ gifts: newGifts });
+  }
+
   render() {
     return (
       <div>
         <h2>Gift Giver</h2>
+        <Button className="btn-add" onClick={this.addGift}>Add Gift</Button>
       </div>
     );
   }
