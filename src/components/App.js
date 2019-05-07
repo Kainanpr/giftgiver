ea3da17105633890d2881560fe1b941825058cc3
Kainan Ramos <Kainan.Ramos@vistajet.com>
2019-05-07 08:57:43 -0300

    [new feature/test] - add a new gift to the rendered list when clicking the add-gift button

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     const { gifts } = this.state;
     const newGifts = [...gifts];
 
-    const nextId = gifts.length > 0 ? gifts.length : 0;
+    const nextId = gifts.length || 0;
 
     newGifts.push({ id: nextId + 1 });
 
@@ -18,9 +18,20 @@ class App extends Component {
   }
 
   render() {
+    const { gifts } = this.state;
+
     return (
       <div>
         <h2>Gift Giver</h2>
+        <div className="gift-list">
+          {
+            gifts.map(gift => (
+              <div key={gift.id}>
+                { gift }
+              </div>
+            ))
+          }
+        </div>
         <Button className="btn-add" onClick={this.addGift}>Add Gift</Button>
       </div>
     );
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -6,18 +6,32 @@ import App from './App';
 
 configure({ adapter: new Adapter() });
 
-const app = shallow(<App />);
+describe('App', () => {
+  const app = shallow(<App />);
 
-it('renders correctly', () => {
-  expect(toJson(app)).toMatchSnapshot();
-});
+  it('renders correctly', () => {
+    expect(toJson(app)).toMatchSnapshot();
+  });
 
-it('initializes the `state` with an empty list of gifts', () => {
-  expect(app.state().gifts).toEqual([]);
-});
+  it('initializes the `state` with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([]);
+  });
 
-it('adds a new gift to `state` when clicking the `add gift` button', () => {
-  app.find('.btn-add').simulate('click');
+  describe('when clicking the `add-gift` button', () => {
+    beforeEach(() => {
+      app.find('.btn-add').simulate('click');
+    });
 
-  expect(app.state().gifts).toEqual([{ id: 1 }]);
+    afterEach(() => {
+      app.setState({ gifts: [] });
+    });
+
+    it('adds a new gift to `state`', () => {
+      expect(app.state().gifts).toEqual([{ id: 1 }]);
+    });
+
+    it('add a new gift to the rendered list', () => {
+      expect(app.find('.gift-list').children().length).toEqual(1);
+    });
+  });
 });
